6da9773cdf19e0da9a5d639433901a7f1d295ebf
Asik8 <motasembillah84@gmail.com>
2024-04-15 15:45:35 +0600

    Nother solved

---
+++ ICPC Assiut University Community/CF-219432Q.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#define lli long long int
+#define max_size 100000
+
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+
+    while(t--)
+    {
+        char n[101];
+        scanf("%s",n);
+        int l = strlen(n)-1;
+        for(int i=l;i>=0;i--)
+        {
+            printf("%c ",n[i]);
+        }printf("\n");
+    }
+
+    return 0;
+}
