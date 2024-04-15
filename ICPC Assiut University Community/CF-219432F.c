f177b1f2d9dd907079dc2bd771ef6cf755dc4c0b
Asik8 <motasembillah84@gmail.com>
2024-04-15 15:20:46 +0600

    Aother solved

---
+++ ICPC Assiut University Community/CF-219432F.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#define lli long long int
+#define max_size 100000
+
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+
+    for (int i=1;i<=12;i++)
+        printf("%d * %d = %d\n",t,i,i*t);
+
+    return 0;
+}
