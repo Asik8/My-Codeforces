77ec9fba5b4bf03e41e6db9f4bd3ae7ed81c397b
Asik8 <motasembillah84@gmail.com>
2024-05-04 10:12:01 +0600

    modified

--- CF-1490A.c
+++ CF-1490A.c
@@ -23,46 +23,33 @@ int main()
             scanf("%d", &arr[i]);
         }
         int tem = arr[0], i = 1, count = 0, min, max;
-        // printf("initial tem: %d\n", tem);
         while (i < n)
         {
             if (tem <= arr[i])
             {
                 min = tem;
                 max = arr[i];
-                // printf("Condition-1 tem: %d arr[i]: %d min=%d max= %d\n", tem, arr[i], min, max);
             }
             else
             {
                 min = arr[i];
                 max = tem;
-                // printf("Condition-2 tem: %d arr[i]: %d min=%d max= %d\n", tem, arr[i], min, max);
             }
             if (max <= (min * 2))
             {
                 tem = arr[i];
                 i++;
-                // printf("Condition-3 Updated_tem: %d i: %d min=%d min*2=%d max= %d\n", tem, i, min, min * 2, max);
             }
             else
             {
-                count++;
-                tem = min * 2;
-                // printf("Condition-4:Before while loop: count=%d updated_tem = %d max=%d min=%d min*2=%d\n", count, tem, max, min, min * 2);
-                if (max > (tem * 2))
+                tem = min;
+                while (max > (tem * 2))
                 {
-                    while (max > (tem * 2))
-                    {
-                        // printf("Condition-4:Inside(starting) while loop: count=%d tem = %d max=%d\n", count, tem, max);
-                        count++;
-                        tem *= 2;
-                        // printf("Condition-4:Inside(Ending) while loop: count=%d tem = %d max=%d\n", count, tem, max);
-                    }
+                    count++;
+                    tem *= 2;
                 }
-                // printf("Condition-4:Outside while loop: count=%d tem = %d max=%d\n", count, tem, max);
                 tem = arr[i];
                 i++;
-                // printf("Condition-4:Ending: count=%d updated_tem = %d arr[i]=%d i=%d\n", count, tem, arr[i], i);
             }
         }
         printf("%d\n", count);
@@ -70,3 +57,17 @@ int main()
 
     return 0;
 }
+
+
+// count++;
+                // tem = min * 2;
+                // if (max > (tem * 2))
+                // {
+                //     while (max > (tem * 2))
+                //     {
+                //         count++;
+                //         tem *= 2;
+                //     }
+                // }
+                // tem = arr[i];
+                // i++;
