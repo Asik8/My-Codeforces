3659e0e2efc7c1316eadfc9dba8124590a9698ed
Asik8 <motasembillah84@gmail.com>
2024-05-10 12:34:40 +0600

    More code added

--- Test.c
+++ Test.c
@@ -1,38 +1,58 @@
-#include<stdio.h>
-#include<math.h>
-#include<string.h>
-#include<ctype.h>
-#include<stdlib.h>
-#include<time.h>
-#include<stdbool.h>
-#include<stddef.h>
-#include<assert.h>
-#include<stdarg.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 #define lli long long int
 #define max_size 100000
 
-int compare(const void *a, const void *b) {
-    return (*(int*)a - *(int*)b);
-}
-
 int main()
 {
-    // int t;
-    // scanf("%d",&t);
+    int n;
+    scanf("%d", &n);
+    int ar[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &ar[i]);
+    }
 
-    // while(t--)
-    // {
-    //     int n;
-    //     scanf("%d",&n);
-    //     int arr[n];
-    //     for (int i = 0; i < n; i++) {
-    //         scanf("%d",&arr[i]);
-    //     }
-    //     qsort(arr, n, sizeof(int), compare);
-    // }
-    lli l=1,h=11;
-    lli mid = l + (h - 1) / 2;
-    printf("%lld\n",mid);
+    int in = (2 * n) / 3;
+    for (int i = 0; i < in - 1; i++)
+    {
+        for (int j = i + 1; j < in; j++)
+        {
+            if (ar[j] > ar[i])
+            {
+                int temp = ar[i];
+                ar[i] = ar[j];
+                ar[j] = temp;
+            }
+        }
+    }
+
+    int in2= (3 * n) / 4;
+    for (int i = n-in2; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (ar[j] < ar[i])
+            {
+                int temp = ar[i];
+                ar[i] = ar[j];
+                ar[j] = temp;
+            }
+        }
+    }
+
+    int ans = n/2;
+
+    printf("%d\n", ar[ans]);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", ar[i]);
+    }
+
     return 0;
 }
