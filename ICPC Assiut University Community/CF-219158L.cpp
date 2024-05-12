74b3ea701df9e47d4af233580f9ff185763c8589
Asik8 <motasembillah84@gmail.com>
2024-05-12 09:39:08 +0600

    12th April 3rd added

---
+++ ICPC Assiut University Community/CF-219158L.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main()
+{
+    string f1,c1,f2,c2;
+    cin >> f1>>c1>>f2>>c2;
+
+    if(c1 == c2) puts("ARE Brothers");
+    else puts("NOT");
+
+    return 0;
+}
