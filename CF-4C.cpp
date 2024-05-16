da6db8e1945c3546277e28016f5ed68dff01219a
Asik8 <motasembillah84@gmail.com>
2024-05-16 12:11:52 +0600

    16th May 1st done

---
+++ CF-4C.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+
+int main() {
+    int t;
+    cin >> t;
+    map<string,int>reg;
+    while (t--) {
+        string s;
+        cin>>s;
+        if(reg[s] == 0)
+        {
+            reg[s] = 1;
+            cout<<"OK\n";
+        }
+        else
+        {
+            cout<<s<<reg[s]<<endl;
+            reg[s]++;
+        }    
+    }
+    return 0;
+}
