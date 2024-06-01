057e7371cb3ce3d221ebfccd421f619fc2f062d3
Asik8 <motasembillah84@gmail.com>
2024-06-01 07:18:21 +0600

    1st June 1st done

---
+++ A_Party.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    int t,c=0;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        if(n>0) c++;
+    }
+    cout<<c<<endl;
+    return 0;
+}
