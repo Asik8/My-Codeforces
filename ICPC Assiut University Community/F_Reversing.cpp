034806f72f7563a866738e2065f635fa461cd11f
Asik8 <motasembillah84@gmail.com>
2024-06-11 16:21:03 +0600

    2nd done

--- ICPC Assiut University Community/F_Reversing.cpp
+++ ICPC Assiut University Community/F_Reversing.cpp
@@ -6,15 +6,10 @@ int main() {
     int n;
     cin>>n;
     vector<int>a(n);
-    vector<int>b(n);
     for(int i=0;i<n;i++)
         cin>>a[i];
-    for(int i=0;i<n;i++)
-        cin>>b[i];
 
-    for(int i=0;i<n;i++)
-        cout<<b[i]<<" ";
-    for(int i=0;i<n;i++)
+    for(int i=n-1;i>=0;i--)
         cout<<a[i]<<" ";
     cout<<endl;
     return 0;
