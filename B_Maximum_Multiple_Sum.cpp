3a016072a160e297cc73ee84bfd9a94983a3c454
Asik8 <motasembillah84@gmail.com>
2024-06-11 21:54:33 +0600

    11th contest 2nd done

---
+++ B_Maximum_Multiple_Sum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if(n == 3) cout<<3<<endl;
+        else cout<<2<<endl;
+    }
+
+    return 0;
+}
