6d196ee02e415eb11b79e4f3db61079233511f95
Asik8 <motasembillah84@gmail.com>
2024-06-18 10:31:05 +0600

    18th june 1st done

--- A_Cut_Ribbon.cpp
+++ A_Cut_Ribbon.cpp
@@ -7,12 +7,17 @@ int main() {
     cin.tie(NULL);
     int a,b,c,n;
     cin>>n>>a>>b>>c;
-    if(a+b ==n || a+c == n || b+c == n) cout<<2<<endl;
-    else if(a+b+c == n) cout<<3<<endl;
-    else
+    int x,y,z,dp[n+1];
+    dp[0] = 0;
+    for(int i=1;i<n+1;i++)
     {
-        
+        x = INT_MIN,y = INT_MIN,z = INT_MIN;
+        if(i>=a) x = dp[i-a];
+        if(i>=b) y = dp[i-b];
+        if(i>=c) z = dp[i-c];
+        dp[i] = 1+max(x,max(y,z));
     }
+    cout<<dp[n]<<endl;
 
     return 0;
 }
