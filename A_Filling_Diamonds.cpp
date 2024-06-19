d5e7492a88543b5419c180dcb1c03b4d6434a168
Asik8 <motasembillah84@gmail.com>
2024-06-19 16:58:32 +0600

    19th june 1st done

---
+++ A_Filling_Diamonds.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+
+    while (t--) {
+        ll n;
+        cin >> n;
+        cout<<n<<endl;
+    }
+
+    return 0;
+}
