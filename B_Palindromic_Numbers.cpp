9ef57517962502480afa2ebca9b52f3ab38fc827
Asik8 <motasembillah84@gmail.com>
2024-06-21 23:39:12 +0600

    some modifications

---
+++ B_Palindromic_Numbers.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pob pop_back
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        string s,ans;
+        cin>>s;
+        if(s[0] != '9')
+        {
+            for(int i=0;i<s.size();i++)
+            {
+                int x = s[i]-'0';
+                int y = 9-x;
+                ans+=(y+'0');
+            }
+        }      
+        else
+        {
+            int c=0;
+            for(int i=n-1;i>= 0;i--)
+            {
+                int x = s[i]-'0';
+                x+=c;
+                if(x>1)
+                {
+                    int y = 11-x;
+                    c = 1;
+                    ans+=(y+'0');
+                }
+                else
+                {
+                    c = 0;
+                    int y = 1-x;
+                    ans+=(y+'0');
+                }
+                reverse(ans.begin(),ans.end());
+            }
+        }   
+        cout<<ans<<endl;   
+    }
+
+    return 0;
+}
