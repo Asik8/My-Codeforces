77ac6b2cf29ea6af3299d466a812e44f8d6c0c8c
Asik8 <motasembillah84@gmail.com>
2024-07-25 23:28:52 +0600

    25 july 2024 1 done

--- B_Large_Addition.cpp
+++ B_Large_Addition.cpp
@@ -7,14 +7,6 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    vector<pair<ll,ll>>v(20,make_pair(0,0));
-    v[0].first = 5;
-    v[0].second = 9;
-    for(int i=1;i<18;i++)
-    {
-        v[i].first = (v[i-1].first)*10+5;
-        v[i].second = (v[i-1].second)*10+9;
-    }
     ll t;
     cin >> t;
     while (t--) {
