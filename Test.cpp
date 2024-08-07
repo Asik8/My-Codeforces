c077d34d2344936f84ea56f0b9be516ba486bc5f
Asik8 <motasembillah84@gmail.com>
2024-08-07 12:59:30 +0600

    test updated

--- Test.cpp
+++ Test.cpp
@@ -4,6 +4,8 @@ using namespace std;
 #define pb push_back
 #define py cout<<"YES\n";
 #define pn cout<<"NO\n";
+#define forni for (int i = 0; i < n; i++)
+#define fornj for (int j = 0; j < n; j++)
 
 int main() {
     ios::sync_with_stdio(false);
@@ -16,6 +18,5 @@ int main() {
         vector <ll> v(n);
         for (auto& x:v) cin >>x;       
     }
-
     return 0;
 }
