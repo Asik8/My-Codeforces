3956ce37a8dde09e535fed76a3bacd6baf18c0eb
Asik8 <motasembillah84@gmail.com>
2024-08-09 22:42:02 +0600

    8th August 2024 1 done

--- E_Triple_Operations.cpp
+++ E_Triple_Operations.cpp
@@ -3,30 +3,35 @@ using namespace std;
 #define ll long long
 #define pb push_back
 
+inline int get_sum_digit(int n)
+{
+    int c = 0;
+    while(n)
+    {
+        c++;
+        n/=3;
+    }
+    return c;
+}
+
 int main() {
+    int N = 2e5+20;
+    int ctr[N],pref[N];
+    for(int i=0;i<N-1;i++)
+    {
+        ctr[i] = get_sum_digit(i);
+        pref[i+1] = pref[i]+ctr[i];
+    }
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     ll t;
     cin >> t;
     while (t--) {
-        ll l,r,c=0;
-        cin >> l>>r;
-        vector <ll> v;
-        for (int i=l;i<=r;i++) v.pb(i);    
-        while(v[0] >0)
-        {
-            v[1]*=3;
-            c++;
-            v[0]/=3;
-        }   
-        for(int i=0;i<v.size();i++)
-        {
-            if(v[i]>0)
-            {
-                c+= static_cast<ll>(floor(log10(v[i]) / log10(3)));
-            }
-        }
-        cout<<c<<endl;
+        ll l,r;
+        cin>>l>>r;
+        r++;
+        ll s = pref[r]-pref[l];
+        cout<<s+ctr[l]<<endl;
     }
 
     return 0;
