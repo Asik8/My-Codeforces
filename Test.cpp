81cffb3c0e33763a6d09a63a74a39a01facd99cc
Asik8 <motasembillah84@gmail.com>
2024-09-05 12:40:03 +0600

    Snippet Updated

--- Test.cpp
+++ Test.cpp
@@ -1,26 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define pb push_back
+
+const int N = 1e5+5;
+vector<int> v[N];
+bool vis[N];
+vector<ll> path;
+ll level[N];
+ll parent[N];
+
+void bfs_Path_Length(ll s,ll d)
+{
+    if(s == d)
+    {
+        cout<<0<<endl;
+        return;
+    }
+    queue<ll>q;
+    q.push(s);
+    level[s] = 0;
+    vis[s] = true;
+    while(!q.empty())
+    {
+        ll n = q.front();
+        q.pop();
+        for(ll x:v[n])
+        {
+            if(!vis[x])
+            {
+                q.push(x);
+                level[x] = level[n]+1;
+                vis[x] = true;
+                parent[x] = n;
+            }
+        }
+    }
+    if(parent[d] == -1) 
+    {
+        cout<<-1<<endl;
+        return;
+    }
+    ll l = d;
+    while(1)
+    {
+        if(l == -1) break;
+        path.pb(l);
+        l = parent[l];
+    }
+    cout<<path.size()-1<<endl;
+    path.clear();
+}
 
 int main() {
-    long long n,value;
-    cin>>n>>value;
-    long long a[n];
-    for(long long i=0;i<n;i++) cin>>a[i];
-    sort(a,a+n);
-    long long l=0,r=n-1;
-    bool f = false;
-    while (l<=r)
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n,e;
+    cin>>n>>e;
+    while(e--)
     {
-        long long mid = (l+r)/2;
-        if(a[mid] == value)
-        {
-            f = true;
-            break;
-        }
-        else if(a[mid]<value) l = mid+1;
-        else if(a[mid]>value) r = mid-1;
-    }  
-    if(f == true) cout<<"YES\n";
-    else cout<<"NO\n";
+        int a,b;
+        cin>>a>>b;
+        v[a].pb(b);
+        v[b].pb(a);
+    }
+    ll q;
+    cin>>q;
+    while(q--)
+    {
+        ll s,d;
+        cin>>s>>d;
+        memset(vis,false,sizeof(vis));
+        memset(parent,-1,sizeof(parent));
+        memset(level,-1,sizeof(level));
+        bfs_Path_Length(s,d);
+    }
     return 0;
 }
