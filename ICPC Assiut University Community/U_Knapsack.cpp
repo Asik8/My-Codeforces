f5f0edd4f715ede3fbdbe990f246a21958315deb
Asik8 <motasembillah84@gmail.com>
2024-10-01 07:38:59 +0600

    1st October 2024 1 done

--- ICPC Assiut University Community/U_Knapsack.cpp
+++ ICPC Assiut University Community/U_Knapsack.cpp
@@ -2,20 +2,23 @@
 using namespace std;
 #define ll long long
 #define pb push_back
+const int N=1000;
+int dp[N][N];
 
 int knapsack(int n,int a[],int b[],int w)
 {
     if(n<0 || w==0) return 0;
+    if(dp[n][w] != -1) return dp[n][w];
     if(a[n]<=w)
     {
         int op1 = knapsack(n-1,a,b,w-a[n])+b[n];
         int op2 = knapsack(n-1,a,b,w);
-        return max(op1,op2);
+        return dp[n][w] = max(op1,op2);
     }
     else 
     {
         int op = knapsack(n-1,a,b,w);
-        return op;
+        return dp[n][w] = op;
     }
 }
 
@@ -26,6 +29,13 @@ int main() {
     cin>>n>>w;
     int a[n],b[n];
     for(int i=0;i<n;i++) cin>>a[i]>>b[i];
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<=w;j++)
+        {
+            dp[i][j] = -1;
+        }
+    }
     cout<<knapsack(n-1,a,b,w)<<endl;
     return 0;
 }
