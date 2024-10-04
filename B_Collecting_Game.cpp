2b105e9b34389f3d5a844888bad3ca70ce6b0ae7
Asik8 <motasembillah84@gmail.com>
2024-10-04 23:22:37 +0600

    4th October 2024 1 done

--- B_Collecting_Game.cpp
+++ B_Collecting_Game.cpp
@@ -11,30 +11,28 @@ int main() {
     while (t--) {
         ll n;
         cin >> n;
-        vector <ll> v(n),a,ans;
-        for (auto& x:v) 
+        vector <pair<ll,ll>> v(n);
+        for (int i=0;i<n;i++) 
         {
-            cin >>x;  
-            a.pb(x);
+            cin>>v[i].first;
+            v[i].second = i;
         }
-        sort(a.begin(),a.end());
-        bool f = false;
-        for(ll b:v)
-        {
-            ll c=0,s=b,in = b;
-            f = false;
-            for(ll x:a)
-            {
-                if(!f && in == x) f = true;
-                else if(s>=x)
-                {
-                    s+=x;
-                    c++;
+        sort(v.begin(),v.end());
+        vector<ll> psum(n);
+        psum[0]= v[0].first;
+        for(int i=1;i<n;i++) psum[i] = psum[i-1]+v[i].first;
+        vector<ll> ans(n,-1);
+        stack<int> st;
+        for(int i=0;i<n;i++){
+           st.push(v[i].second);
+           if(i==n-1 or v[i+1].first>psum[i]){
+                while(!st.empty()){
+                   ans[st.top()]=i;
+                   st.pop();
                 }
-            }
-            ans.pb(c);
-        }     
-        for(ll a:ans) cout<<a<<" ";
+           }
+        }
+        for(ll val:ans) cout<<val<<" ";
         cout<<endl;
     }
     return 0;
