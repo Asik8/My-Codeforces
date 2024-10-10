058bc5c3cb2e120c0012086689cdbf77349008ac
Asik8 <motasembillah84@gmail.com>
2024-10-10 10:27:16 +0600

    9th October 2024 1 done

--- C_Yarik_and_Array.cpp
+++ C_Yarik_and_Array.cpp
@@ -9,19 +9,19 @@ int main() {
     ll t;
     cin >> t;
     while (t--) {
-        ll n,p,m=INT_MIN,a,s=0;
+        ll n,par,m=INT_MIN,a,s=0;
         cin >> n;
         cin>>a;
-        p = abs(a)%2; 
+        par = abs(a)%2; 
         s=a;
         m = s;
         for(int i=1;i<n;i++)
         {
             cin>>a;
-            if(abs(a)%2 != p) s+=a;
+            if(abs(a)%2 != par) s+=a;
             else s = a;
             if(a>s) s = a;
-            p = abs(a)%2;
+            par = abs(a)%2;
             m = max(m,s);
         }     
         cout<<m<<endl;
