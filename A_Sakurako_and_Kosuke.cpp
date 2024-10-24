a025a1f969054a3736482d9bd9e05095f4a257a5
Asik8 <motasembillah84@gmail.com>
2024-10-24 22:56:20 +0600

    24th October 2024 3 done

---
+++ A_Sakurako_and_Kosuke.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"Sakurako\n";
+#define pn cout<<"Kosuke\n";
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        if(!(n%2)) py
+        else pn
+    }
+    return 0;
+}
