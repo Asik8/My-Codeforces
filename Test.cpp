81ca3635591c5996d95559abd7e418cb7fb38726
Asik8 <motasembillah84@gmail.com>
2024-10-29 08:05:49 +0600

    29 October 2024 1 done

--- Test.cpp
+++ Test.cpp
@@ -4,12 +4,10 @@ using namespace std;
 int main() {
    ios::sync_with_stdio(false);
    cin.tie(nullptr);
-
-    pair<string, int> student = { "dablu", 10 };
-    cout << student.first << " " << student.second << '\n';
-    student.first = "bablu";
-
-    auto [name, roll] = student;
-    cout << name << " " << roll << '\n';
+   pair<string, int> student = { "dablu", 10 };
+   cout << student.first << " " << student.second << '\n';
+   student.first = "bablu";
+   for(auto[name, roll]:student)
+      cout << name << " " << roll << '\n';
    return 0;
 }
