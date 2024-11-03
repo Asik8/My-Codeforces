df64d496f68bc46fe9c934857ff7bb78fe7afb4e
Asik8 <motasembillah84@gmail.com>
2024-11-03 21:42:35 +0600

    3rd November 2024 3 done

--- C_Anya_and_1100.cpp
+++ C_Anya_and_1100.cpp
@@ -5,6 +5,10 @@ using namespace std;
 #define py cout<<"YES\n";
 #define pn cout<<"NO\n";
 
+bool valid(const string &s,ll i){
+    return i>=0 && i+3<s.size() && s[i]=='1' && s[i+1]=='1' && s[i+2]=='0' && s[i+3]=='0';
+}
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -13,26 +17,17 @@ int main(){
     while (t--){
         string s;
         cin>>s;
-        ll q;
+        ll q,c=0;
+        for(int i=0;i<s.size()-3;i++) if(valid(s,i))c++;
         cin >> q;
         while(q--){
             int i; char v;
             cin>>i>>v;
             i--;
-            if(s.size()<4) pn
-            else{
-                ll c=0;
-                s[i] = v;
-                bool f = false;
-                for(int i=0;i<s.size()-3;i++){
-                    if(s[i]=='1' && s[i+1]=='1' && s[i+2]=='0'&&s[i+3]=='0'){
-                        f = true;
-                        break;
-                    }
-                }
-                if(f) py
-                else pn
-            }
+            for(int j=i-3;j<=i;j++) if(valid(s,j)) c--;
+            s[i]=v;
+            for(int j=i-3;j<=i;j++) if(valid(s,j)) c++;
+            if(c) py else pn
         }       
     }
     return 0;
