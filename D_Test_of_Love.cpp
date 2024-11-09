1908ff82044849a8f326cf85111159bfd843ae03
Asik8 <motasembillah84@gmail.com>
2024-11-09 22:09:08 +0600

    9th November 2024 1 done

--- D_Test_of_Love.cpp
+++ D_Test_of_Love.cpp
@@ -12,10 +12,31 @@ int main(){
     ll t;
     cin >> t;
     while (t--){
-        ll n;
-        cin >> n;
-        vector <char> v(n+1);
-        f(i,1,n+1) cin>>v[i];       
+        ll n,m,k;
+        cin >>n>>m>>k;
+        vector <char> s(n+2);
+        s[0]='!';
+        s[n+1]='!';
+        f(i,1,n+1) cin>>s[i];
+        vector<ll>in;
+        in.pb(0);    
+        f(i,0,n+2) if(s[i]=='L') in.pb(i);
+        in.pb(n+1);
+        bool f=true;
+        f(i,0,in.size()-1){
+            if(in[i+1]-in[i]>(m+k)){
+                f = false;
+                break;
+            }
+        }
+        if(f){
+            ll l=in[in.size()-2];
+            f(i,l+m,min(l+k+1,n+2)) if(s[i]=='C'){
+                f=false;
+                break;
+            }
+        }
+        if(f)py else pn
     }
     return 0;
 }
