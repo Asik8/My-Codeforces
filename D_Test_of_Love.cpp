63e77ad4d43b88b4a7656782e1c9e2cbfcebd39e
Asik8 <motasembillah84@gmail.com>
2024-11-09 12:15:24 +0600

    Snippet Updated

---
+++ D_Test_of_Love.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define f(x1,x2,x3) for (int x1 = x2; x1 < x3; x1++)
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--){
+        ll n;
+        cin >> n;
+        vector <char> v(n+1);
+        f(i,1,n+1) cin>>v[i];
+             
+    }
+    return 0;
+}
