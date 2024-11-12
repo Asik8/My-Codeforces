2522ab56c28f41c195532cc6d49f74b863a47b1b
Asik8 <motasembillah84@gmail.com>
2024-11-12 14:49:37 +0600

    12th November 2024 3 done

--- B_Number_of_Smaller.cpp
+++ B_Number_of_Smaller.cpp
@@ -8,17 +8,19 @@ using namespace std;
 #define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
 #define forni for(int i=0;i<n;i++)
 
-
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    ll t;
-    cin >> t;
-    while (t--){
-        ll n;
-        cin >> n;
-        vector <ll> v(n);
-        for (auto& x:v) cin >>x;       
+    ll n,m;
+    cin>>n>>m;
+    vector<ll>a(n),b(m),ans;
+    for(auto &x:a) cin>>x;
+    for(auto &x:b) cin>>x;
+    ll l=0;
+    for(auto x:b){
+        while(a[l]<x && l<n)l++;
+        ans.pb(l);
     }
+    for(auto x:ans)cout<<x<<" ";
     return 0;
 }
