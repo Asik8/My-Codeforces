86c181d6afffe9ef7be48df6a0f9c40d64944cbf
Asik8 <motasembillah84@gmail.com>
2024-11-17 21:54:39 +0600

    17th November 2024 6 done

--- C_Superultra_s_Favorite_Permutation.cpp
+++ C_Superultra_s_Favorite_Permutation.cpp
@@ -7,16 +7,16 @@ using namespace std;
 #define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
 #define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
 #define forni for(int i=0;i<n;i++)
+const ll N=2e5+20;
+bool ch[N];
 
 bool check(ll n){
-    bool f=false;
-    for(int i=2;i<=sqrt(n);i++){
+    for(int i=2;i*i<=n;i++){
         if(!(n%i)){
-            f= true;
-            break;
+            return true;
         }
     }
-    return f;
+    return false;
 }
 
 int main() {
@@ -27,25 +27,35 @@ int main() {
     while (t--) {
         ll n;
         cin >> n;
-        vector <ll> v(n);
-        ll l=2,r=n-1;
-        v[0]=1;
-        v[1]=n;
-        for(int i=2;i<n;i++){
-            if(i%2) v[i]=r--;
-            else v[i]=l++;
+        vector <ll> v;
+        v.pb(1);
+        ll l=3;
+        while(l<=n){
+            v.pb(l);
+            l+=2;
         }
-        bool f=true;
-        for(int i=0;i<n-1;i++){
-            if(!(check(v[i]+v[i+1]))){
-                f =false;
+        l-=2;
+        memset(ch,false,sizeof(ch));
+        ll r=2;
+        bool f=false;
+        while(r<=n){
+            if(check(l+r)){
+                f=true;
+                ch[r]=true;
+                v.pb(r);
                 break;
             }
+            r+=2;
         }
         if(!f) cout<<"-1\n";
         else{
+            r=2;
+            while(r<=n){
+                if(!ch[r]) v.pb(r);
+                r+=2;
+            }
             for(auto c:v) cout<<c<<" ";
-            cout<<endl;       
+            cout<<endl;
         }
     }
     return 0;
