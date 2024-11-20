a06090deb6635c7dd534ae151916623a76f47625
Asik8 <motasembillah84@gmail.com>
2024-11-20 18:01:00 +0600

    20th November 2024 1 done

---
+++ D_Number_of_Segments_with_Big_Sum.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n,k;
+    cin >> n>>k;
+    vector <ll> v(n);
+    for (auto& x:v) cin >>x; 
+    ll c=0,s=0,l=0,r=0;
+    while(r<n){
+        s+=v[r];
+        while(s>=k) s-=v[l++];
+        c+=l;
+        r++;
+    }
+    cout<<c<<endl;
+    return 0;
+}
