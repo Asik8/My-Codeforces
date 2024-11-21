3f16d0822bdb43bd10c810fa362579379a99bdb8
Asik8 <motasembillah84@gmail.com>
2024-11-21 10:59:03 +0600

    21th November 2024 3 done

--- D_Right_Left_Wrong.cpp
+++ D_Right_Left_Wrong.cpp
@@ -4,43 +4,37 @@ using namespace std;
 #define pb push_back
 #define py cout<<"YES\n";
 #define pn cout<<"NO\n";
-#define forni for (int i = 0; i < n; i++)
-#define fornj for (int j = 0; j < n; j++)
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  int tt;
-  cin >> tt;
-  while (tt--) {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) {
-      cin >> a[i];
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        vector <ll> v(n),p(n),lp,rp;
+        for (auto& x:v) cin >>x;    
+        p[0]=v[0];
+        for(int i=1;i<n;i++) p[i]=v[i]+p[i-1]; 
+        string s;
+        cin>>s;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='L') lp.pb(i);
+            if(s[i]=='R') rp.pb(i);
+        }
+        reverse(rp.begin(),rp.end());
+        ll c=0;
+        for(int i=0;i<min(lp.size(),rp.size());i++){
+            if(lp[i]<rp[i]){
+                if(lp[i]==0) c+=p[rp[i]];
+                else c+=p[rp[i]]-p[lp[i]-1];
+            }
+        } 
+        cout<<c<<endl;
     }
-    string s;
-    cin >> s;
-    vector<int> coeff(n, n + 1);
-    {
-      int cnt = 0;
-      for (int i = 0; i < n; i++) {
-        cnt += (s[i] == 'L');
-        coeff[i] = min(coeff[i], cnt);
-      }
-    }
-    {
-      int cnt = 0;
-      for (int i = n - 1; i >= 0; i--) {
-        cnt += (s[i] == 'R');
-        coeff[i] = min(coeff[i], cnt);
-      }
-    }
-    int64_t ans = 0;
-    for (int i = 0; i < n; i++) {
-      ans += int64_t(coeff[i]) * a[i];
-    }
-    cout << ans << '\n';
-  }
-  return 0;
+    return 0;
 }
