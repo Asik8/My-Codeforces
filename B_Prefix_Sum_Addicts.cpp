cb5819d97c4877775982cd8df3f6d7d2f4139ede
Asik8 <motasembillah84@gmail.com>
2024-11-27 18:13:36 +0600

    27th November 2024 4 done

--- B_Prefix_Sum_Addicts.cpp
+++ B_Prefix_Sum_Addicts.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 #define ll long long
 #define pb push_back
-#define py cout<<"YES\n";
-#define pn cout<<"NO\n";
+#define py cout<<"Yes\n";
+#define pn cout<<"No\n";
 #define co(x1) cout<<x1<<"\n";
 #define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
 #define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
@@ -15,10 +15,24 @@ int main() {
     ll t;
     cin >> t;
     while (t--) {
-        ll n;
-        cin >> n;
-        vector <ll> v(n);
-        for (auto& x:v) cin >>x;       
+        ll n,k;
+        cin>>n>>k;
+        vector <ll> v(n+1),a(n+1);
+        for(int i=n-k+1;i<=n;i++) cin>>v[i];
+        if(k==1){
+            py
+            continue;
+        }   
+        for(int i=n-k+2;i<=n;i++) a[i]=v[i]-v[i-1];
+        if(!is_sorted(a.begin()+n-k+2,a.end())){
+            pn
+            continue;
+        }
+        if(v[n-k+1]>a[n-k+2]*(n-k+1)) {
+            pn
+            continue;
+        }
+        py
     }
     return 0;
 }
