c43fb5d8d2c8f2b96eb27327a0a49cfc118be48f
Asik8 <motasembillah84@gmail.com>
2024-12-15 20:42:54 +0600

    15th December 2024 1 done

---
+++ A_Easy_Problem.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define co(x1) cout<<x1<<"\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        ll b=1,a=5,c=0;
+        for(int i=n;i>0;i--){
+            for(int j=1;j<=n;j++){
+                if(i==(n-j)) c++;
+            }
+        }
+        co(c)     
+    }
+    return 0;
+}
