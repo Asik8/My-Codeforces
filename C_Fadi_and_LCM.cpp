fe7c24fbfddb8cf52b7181e262e8fdf841d4c5d9
Asik8 <motasembillah84@gmail.com>
2024-12-25 09:56:36 +0600

    25th December 2024 2 done

--- C_Fadi_and_LCM.cpp
+++ C_Fadi_and_LCM.cpp
@@ -15,21 +15,30 @@ ll LCM(ll a,ll b){
     return (a*(b/__gcd(a,b)));
 }
 
-
 void asikM(){
     ll n;
     cin >> n;
-    vector <ll> v(n);
-    for (auto& x:v) cin >>x; 
+    ll ans=n,a=1,b=n;
+    for(ll i=1;i*i<=n;i++){
+        if(!(n%i)){
+            ll a1=i,a2=n/i;
+            if(LCM(a1,a2) == n && max(a1,a2)<ans){
+                ans=max(a1,a2);
+                a=a1;
+                b=a2;
+            }
+        }
+    }
+    cout<<a<<" "<<b<<endl;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    ll t;
-    cin >> t;
-    while (t--) {
+    // ll t;
+    // cin >> t;
+    // while (t--) {
         asikM();      
-    }
+    // }
     return 0;
 }
