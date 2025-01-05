f5e3917f1162a74c2b36fb3fd0aeed5a03dc0829
Asik8 <motasembillah84@gmail.com>
2025-01-05 08:06:57 +0600

    5th January 2025 2 done

---
+++ A_Line_Trip.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define co(x1) cout<<x1<<"\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define sz(x) x.size()
+#define vec vector<ll>
+
+void asikM(){
+    ll n,m;
+    cin >>n>>m;
+    vector <ll> v(n);
+    for (auto& x:v) cin >>x; 
+    ll dis=v[0];
+    f(i,1,n) dis=max(dis,v[i]-v[i-1]);
+    dis=max(dis,(m-v[n-1])*2);
+    co(dis)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
