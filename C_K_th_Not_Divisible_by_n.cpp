575699db5e0a7b0ba942ed46e6e7d952f6142e58
Asik8 <motasembillah84@gmail.com>
2025-01-06 14:03:49 +0600

    6th January 2025 2 done

---
+++ C_K_th_Not_Divisible_by_n.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define co(x1) cout<<x1<<"\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define sz(x) x.size()
+#define vec vector<ll>
+
+void asikM(){
+    ll n,k,ans;
+    cin >> n>>k;
+    if(k<n) ans=(k);
+    else{
+        ll x=k+(k/(n-1));
+        if(x%n) ans=x;
+        else ans=x-1;
+    }
+    co(ans)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
