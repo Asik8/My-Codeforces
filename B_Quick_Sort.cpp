7f2ea27de300fe957b6515dedc0a7e2bbecf616f
Asik8 <motasembillah84@gmail.com>
2025-01-11 20:04:39 +0600

    11th January 2025 2 done

---
+++ B_Quick_Sort.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define co(x1) cout<<x1<<"\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define sz(x) x.size()
+#define vec vector<ll>
+
+void asikM(){
+    ll n,k;
+    cin >> n>>k;
+    vector <ll> v(n);
+    for (auto& x:v) cin >>x; 
+    ll c=1;
+    for(auto x:v) if(x==c) c++;
+    co((n-c+k)/k)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
