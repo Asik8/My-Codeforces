b3431d13f8022ee14c66524a38b725ad1d19e220
Asik8 <motasembillah84@gmail.com>
2025-01-17 16:43:42 +0600

    17th January 2024 3 done

---
+++ B_T_primes.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define co(x1) cout<<x1<<"\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define sz(x) x.size()
+#define vec vector<ll>
+const int N=1e6+20;
+vector<int>vis(N+1,1);
+
+void asikM(){
+    ll t;
+    cin >> t;
+    while (t--){
+        ll n;
+        cin >> n;
+        if(n<4){
+            pn
+            return;
+        }
+        ll s=sqrt(n);
+        if(s*s==n && vis[s]) py else pn
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    vis[0]=0;
+    vis[1]=0;
+    for(ll i=2;i<=N;i++){
+        if(vis[i]==1){
+            for(ll j=i+i;j<=N;j+=i){
+                vis[j]=0;
+            }
+        }
+    }
+    asikM();      
+    return 0;
+}
