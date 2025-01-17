d30e40bcd2fef326ba049c561106d06b7a661a34
Asik8 <motasembillah84@gmail.com>
2025-01-17 21:22:38 +0600

    17th January 2024 5 done

---
+++ A_Shape_Perimeter.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define co(x1) cout<<x1<<"\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define sz(x) x.size()
+#define vec vector<ll>
+
+void asikM(){
+    ll n,l;
+    cin >> n>>l;
+    ll s1=l,s2=l;
+    forni{
+        ll x,y;
+        cin>>x>>y;
+        if(i>0){
+            s1+=x;
+            s2+=y;
+        }
+    }
+    co((s1+s2)*2)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
