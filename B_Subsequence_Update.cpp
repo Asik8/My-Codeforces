c36feac8f8af545d0831daa68ffee07d3578453d
Asik8 <motasembillah84@gmail.com>
2025-01-22 20:18:20 +0600

    22th January 2025 4 done

--- B_Subsequence_Update.cpp
+++ B_Subsequence_Update.cpp
@@ -8,6 +8,7 @@ using namespace std;
 #define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
 #define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
 #define forni for(int i=0;i<n;i++)
+#define all(x) x.begin(),x.end()
 #define sz(x) x.size()
 #define vec vector<ll>
 
@@ -16,47 +17,18 @@ void asikM(){
     cin>>n>>l>>r;
     vector <ll> v(n);
     for (auto& x:v) cin >>x; 
-    l--;r--;
-    ll s=0,mx=LLONG_MIN,s1=0,s2=0,mx_in=-1;
-    for(int i=l;i<=r;i++){
-        s+=v[i];
-        if(v[i]>mx){
-            mx=max(mx,v[i]);
-            mx_in=i;
-        }
+    ll ran=(r-l+1);
+    vec a,b;
+    f(i,0,r) a.pb(v[i]);
+    f(i,l-1,n) b.pb(v[i]);
+    sort(all(a));
+    sort(all(b));
+    ll s1=0,s2=0;
+    f(i,0,ran){
+        s1+=a[i];
+        s2+=b[i];
     }
-    ll in1=-1,in2=-1;
-    if(l==0 && r==n-1){
-        co(s)
-        return;
-    }
-    if(l>0){
-        ll mx1=mx;
-        for(int i=l-1;i>=0;i--){
-            if(v[i]<mx1){
-                in1=i;
-                mx1=v[i];
-            }
-        }
-        vec a=v;
-        if(in1 !=-1)reverse(a.begin()+in1,a.begin()+mx_in+1);
-        for(auto x:a) cout<<x<<" ";
-        cout<<endl;
-        a.clear();
-    }
-    if(r<n-1){
-        ll mx2=mx;
-        for(int i=r+1;i<n;i++){
-            if(v[i]<mx2){
-                in2=i;
-                mx2=v[i];
-            }
-        }
-        vec a=v;
-        if(in2 !=-1)reverse(a.begin()+mx_in,a.begin()+in2+1);
-        for(auto x:a) cout<<x<<" ";
-        cout<<endl;
-    }
+    co(min(s1,s2))
 }
 
 int main() {
