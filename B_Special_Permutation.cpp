eae6ab856c38620f78233162030649cf1d69a90a
Asik8 <motasembillah84@gmail.com>
2025-01-23 08:43:41 +0600

    snippet Updated

---
+++ B_Special_Permutation.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define py cout<<"YES\n";
+#define pn cout<<"NO\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec vector<ll>
+
+void asikM(){
+    ll n,a,b;
+    cin >> n>>a>>b;
+    vector <ll> v(n);
+    for (auto& x:v) cin >>x; 
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
