0f4b0685457e14639400553689c775071bc7e7c4
Asik8 <motasembillah84@gmail.com>
2025-01-27 11:35:25 +0600

    27th January 2025 1 done

---
+++ C_Cirno_and_Operations.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n;
+    cin >> n;
+    vector <ll> v(n);
+    for (auto& x:v) cin >>x; 
+    if(n==1){
+        co(v[0])
+        return;
+    }
+    ll s=accumulate(all(v),0LL);
+    vec(ll) a;
+    while(n>1){
+        for(int i=0;i<n-1;i++) a.pb(v[i+1]-v[i]);
+        s=max(s,abs(accumulate(all(a),0LL)));
+        for(int i=0;i<n-1;i++) v[i]=a[i];
+        a.clear();
+        n--;
+    }
+    co(s)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
