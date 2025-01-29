0bc286e1380431f0040be72bf003406c8f9c5c94
Asik8 <motasembillah84@gmail.com>
2025-01-29 13:00:44 +0600

    29th January 2025 1 done

---
+++ C_Cows_in_Stalls.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n,k;
+    cin >> n>>k;
+    vector <ll> v(n);
+    for (auto& x:v) cin >>x;
+    auto ch=[&](ll m){
+        ll c=1,in=0;
+        f(i,1,n){
+            if(v[i]-v[in]>=m){
+                c++;
+                in=i;
+            }
+        }
+        return c>=k;
+    };
+    ll l=0,r=1e9+5,m;
+    while(l+1<r){
+        m=(l+r)/2;
+        if(ch(m)) l=m;
+        else r=m;
+    }
+    co(l)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    // cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
