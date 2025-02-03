cd74423943f42dcfef925040212bbad6ade47a08
Asik8 <motasembillah84@gmail.com>
2025-02-03 23:01:25 +0600

    3rd February 2025 2 done

---
+++ C_Customer_Service.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n;
+    cin >> n;
+    ll a[n][n];
+    f(i,0,n) f(j,0,n) cin>>a[i][j];
+    vec(ll)v;
+    f(i,0,n){
+        ll c=0;
+        for(int j=n-1;i>=0&& a[i][j]==1;j--) c++;
+        v.pb(c);
+    }
+    sort(all(v));
+    ll ans=0;
+    for (auto x:v) ans+=(x>=ans);
+    co(ans)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
