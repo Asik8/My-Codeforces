c47377c6a3a852a87431f6f89b7f02f245d326c0
Asik8 <motasembillah84@gmail.com>
2025-02-15 16:52:51 +0600

    15th February 2025 1 doen

---
+++ C_Devyatkino.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n,ans=10;
+    cin >> n;
+    auto ch=[&](ll n){
+        while(n){
+            ll x=n%10;
+            n/=10;
+            if(x==7) return true;
+        }
+        return false;
+    };
+    for(ll i=9;i<10000000000;i=i*10+9){
+        ll x=n,c=0;
+        while(!ch(x)){
+            c++;
+            x+=i;
+        }
+        ans=min(ans,c);
+    }
+    co(ans)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
