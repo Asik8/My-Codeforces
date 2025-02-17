e18c7b966282393ead346929b9c051141fc2dc47
Asik8 <motasembillah84@gmail.com>
2025-02-17 10:09:58 +0600

    17th February 2025 2 done

---
+++ C_Remove_the_Ends.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n,ps=0,ns=0;
+    cin >> n;
+    vector <ll> v(n),pos(n),neg(n);
+    for (auto& x:v) cin >>x; 
+    forni{
+        if(v[i]>0) ps+=v[i];
+        else ns-=v[i];
+        pos[i]=ps;
+        neg[i]=ns;
+    }
+    ll ans=max(ps,ns);
+    forni
+        ans=max(ans,pos[i]+ns-neg[i]);
+    co(ans)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
