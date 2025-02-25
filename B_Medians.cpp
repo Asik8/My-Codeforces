be68906f0eef11bdc4c1130eb3e23340cf13b15f
Asik8 <motasembillah84@gmail.com>
2025-02-25 09:28:45 +0600

    25th February 2025 1 done

---
+++ B_Medians.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n,k;
+    cin >> n>>k;
+    if(n==k && n==1){
+        co(1)
+        co(1)
+        return;
+    }
+    if(k==1 || k==n){
+        co(-1)
+        return;
+    }
+    if(!(k&1)){
+        co(3)
+        cout<<1<<" "<<k<<" "<<k+1<<el
+    }
+    else if((k&1)){
+        co(3)
+        cout<<1<<" "<<k-1<<" "<<k+2<<el
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
