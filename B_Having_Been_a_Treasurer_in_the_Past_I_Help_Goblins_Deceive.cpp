47694a669cb0d537e9b684a0ce52c2a1915787cb
Asik8 <motasembillah84@gmail.com>
2025-02-25 20:55:29 +0600

    25th February 2025 4 done

---
+++ B_Having_Been_a_Treasurer_in_the_Past_I_Help_Goblins_Deceive.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n;
+    cin >> n;
+    string s;
+    cin>>s;
+    map<char,ll>m;
+    for(auto x:s) m[x]++;
+    ll c1=0,c2=0,c3=0;
+    if(m['-']&1) c3=((m['-']+1)/2);
+    else c3=m['-']/2;
+    c2=m['_'];
+    c1=m['-']/2;
+    co(c1*(c2*c3))
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
