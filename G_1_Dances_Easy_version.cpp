71ceef2a71dc8202e40b17af70fa49628863951a
Asik8 <motasembillah84@gmail.com>
2025-03-02 12:39:33 +0600

    2nd March 2025 2 done

---
+++ G_1_Dances_Easy_version.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n,m;
+    cin >> n>>m;
+    vector <ll> a(n),b(n);
+    a[0]=1;
+    f(i,1,n) cin >>a[i]; 
+    for (auto& x:b) cin >>x; 
+    sort(all(a));
+    sort(all(b));
+    ll c=0,l=n-1,r=n-1;
+    while(l>=0){
+        if(a[l]<b[r]){
+            l--;
+            r--;
+        }
+        else{
+            l--;
+            c++;
+        }
+    }
+    co(c)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
