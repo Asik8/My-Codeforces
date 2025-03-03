4bc4f419077271f20320d44150f76310f9746768
Asik8 <motasembillah84@gmail.com>
2025-03-03 07:01:28 +0600

    3rd March 2025 1 done

---
+++ A_Satisfying_Constraints.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define f(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define fr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n,l=1,r=1e9;
+    cin >> n;
+    set<ll>s;
+    forni{
+        ll x,y;
+        cin>>x>>y;
+        if(x==1 && y>l) l=y;
+        if(x==2 && y<r) r=y;
+        if(x==3) s.insert(y);
+    }
+    ll c=max(0LL,r-l+1);
+    for(auto x:s) c-=(l<=x && r>=x);
+    co(c)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
