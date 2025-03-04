a149ef6e254e6d13b24c6ed2380277a24ab72bf9
Asik8 <motasembillah84@gmail.com>
2025-03-04 22:46:46 +0600

    4th March 2025 2 done

---
+++ C_Set_Construction.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n;
+    cin >> n;
+    string s[n];
+    forni cin>>s[i];
+    set<ll> st[n+1];
+    fl(i,1,n+1) st[i].insert(i);
+    forni{
+        fl(j,0,n){
+            if(s[i][j]=='1'){
+                st[j+1].insert(i+1);
+            }
+        }
+    }
+    fl(i,1,n+1){
+        ct(sz(st[i]))
+        flx(st[i])
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
