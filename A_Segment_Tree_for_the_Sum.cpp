a620362d02dbf0527b7c9063ffd689c2271a39dc
Asik8 <motasembillah84@gmail.com>
2025-03-09 22:57:48 +0600

    9th March 2025 1 done

---
+++ A_Segment_Tree_for_the_Sum.cpp
@@ -0,0 +1,160 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define ll long long
+// #define pb push_back
+// #define pbs insert
+// #define pi pair<ll,ll>
+// #define py cout<<"YES\n";
+// #define pys cout<<"Yes\n";
+// #define pn cout<<"NO\n";
+// #define pns cout<<"No\n";
+// #define co(x1) cout<<x1<<"\n";
+// #define ct(x1) cout<<x1<<" ";
+// #define elc cout<<"\n";
+// #define el "\n";
+// #define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+// #define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+// #define flx(x1) for(auto x:x1) ct(x) elc
+// #define forni for(int i=0;i<n;i++)
+// #define all(x1) x1.begin(),x1.end()
+// #define allr(x1) x1.rbegin(),x1.rend()
+// #define sz(x) x.size()
+// #define vec(x) vector<x>
+
+// const int N=1e5+5;
+// int a[N],t[4*N];
+
+// void build(int n,int b,int e){
+//     if(b==e){
+//         t[n]=a[b];
+//         return;
+//     }
+//     int m=(b+e)/2,l=n*2,r=(n*2)+1;
+//     build(l,b,m);
+//     build(r,m+1,e);
+//     t[n]=t[l]+t[r];
+// }
+
+// void update(int n,int b,int e,int i,int v){
+//     if(i<b || i>e) return;
+//     if(b==e){
+//         t[n]=v;
+//         return;
+//     }
+//     int m=(b+e)/2,l=(n*2),r=(n*2)+1;
+//     update(l,b,m,i,v);
+//     update(r,m+1,e,i,v);
+//     t[n]=t[l]+t[r];
+// }
+
+// void asikM(){
+//     ll n,m;
+//     cin >> n>>m;
+//     fl(i,1,n+1) cin>>a[i]; 
+// }
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     ll t=1;
+//     // cin >> t;
+//     while (t--)
+//     asikM();      
+//     return 0;
+// }
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+const int N=1e5+9;
+ll a[N],t[4*N];
+
+void build(int n,int b,int e){
+    if(b==e){
+        t[n]=a[b];
+        return;
+    }
+    int m=(b+e)/2,l=(n*2),r=(n*2)+1;
+    build(l,b,m);
+    build(r,m+1,e);
+    t[n]=t[l]+t[r];
+}
+
+void update(int n,int b,int e,int i,int v){
+    if(i<b || i>e) return;
+    if(b==e){
+        t[n]=v;
+        return;
+    }
+    int m=(b+e)/2,l=(n*2),r=(n*2)+1;
+    update(l,b,m,i,v);
+    update(r,m+1,e,i,v);
+    t[n]=t[l]+t[r];
+}
+
+ll query(int n, int b, int e, int i, int j) {
+    if (e < i || j < b) {
+       return 0;
+    }
+    if (b >= i && e <= j) {
+       return t[n];
+    }
+    int mid = (b + e) / 2, l = (2 * n), r = (2 * n) + 1;
+    return query(l, b, mid, i, j) + query(r, mid + 1, e, i, j);
+ }
+ 
+
+void asikM(){
+    ll n,m;
+    cin >> n>>m;
+    fl(i,1,n+1) cin>>a[i]; 
+    build(1,1,n);
+    while(m--){
+        int t;
+        cin >> t;
+        if (t == 1) {
+           int idx, v;
+           cin >> idx >> v;
+           idx++;
+           update(1, 1, n, idx, v);
+        }
+        else {
+           int l, r;
+           cin >> l >> r;
+           l++;
+           ll ans = query(1, 1, n, l, r);
+           cout << ans << '\n';
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    // cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
