9adc7be1bebeddb9d970ff34e577f1e0c51e7b23
Asik8 <motasembillah84@gmail.com>
2025-03-12 20:51:42 +0600

    12th March 2025 1 done

---
+++ C_XOR_and_Triangle.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    int x;
+    cin >> x;
+    if(__builtin_popcountll(x) == 1){
+        co(-1)
+        return;
+    }
+    int y=0,f1=0,f0=0;
+    fl(i,0,31){
+        if(x&(1<<i)){
+            if(f1==0){
+                y|= 1<<i;
+                f1=1;
+            }
+        } else{
+            if(f0==0){
+                y|=1<<i;
+                f0=1;
+            }
+        }
+    }
+    if(y<x) co(y) else co(-1)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
