fba94f97ae70d7ca22e5b5e8a6f513d80361494d
Asik8 <motasembillah84@gmail.com>
2025-03-17 20:49:40 +0600

    17th March 2025 1 done

--- Test.cpp
+++ Test.cpp
@@ -1,43 +1,12 @@
-#include <bits/stdc++.h>
-#define need_speed ios_base::sync_with_stdio(0), cin.tie(0)
-using namespace std;
+#include <stdio.h>
+#include <string.h>
 
-int main(void)
-{
-    need_speed;
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        string s;
-        cin >> s;
-        stack<char> st;
-        bool isBalanced = true;  
-        for (auto x : s)
-        {
-            if (x == '{' || x == '(' || x == '[')
-                st.push(x);  
-            else if (x == '}' || x == ')' || x == ']')
-            {
-                if (st.empty()) 
-                {
-                    isBalanced = false;  
-                    break;
-                }
-                if (x == ')' && st.top() == '(')
-                    st.pop(); 
-                else if (x == '}' && st.top() == '{')
-                    st.pop(); 
-                else if (x == ']' && st.top() == '[')
-                    st.pop(); 
-                else
-                {
-                    isBalanced = false;  
-                    break;
-                }
-            }
-        }
-        if (!st.empty()) isBalanced = false;
-        cout << (isBalanced ? "Balanced" : "Not Balanced") << endl;
-    }
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';
+    int length = strlen(str);
+    printf("The length of the string is: %d\n", length);
+    return 0;
 }
