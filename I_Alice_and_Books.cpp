d401355abd3d45eeb560450455700836c7968157
Asik8 <motasembillah84@gmail.com>
2025-03-25 00:11:54 +0600

    25th March 2025 1 done

---
+++ I_Alice_and_Books.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n;
+    cin >> n;
+    vector <ll> v(n);
+    for (auto& x:v) cin >>x; 
+    stack<ll>s1,s2;
+    s1.push(v[0]);
+    s2.push(v[1]);
+    fl(i,2,n){
+        if(s1.top()==s2.top()) s1.push(v[i]);
+        else if(s1.top()>s2.top()) s2.push(v[i]);
+        else if(s1.top()<s2.top()) s1.push(v[i]);
+    }
+    co(s1.top()+s2.top())
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
