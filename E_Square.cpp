9cbf2dc96f56422a0d5752f89f97fff7aa5a863f
Asik8 <motasembillah84@gmail.com>
2025-03-24 14:31:02 +0600

    24th March 2025 1 done

---
+++ E_Square.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll a1,b1,a2,b2;
+    cin>>a1>>b1>>a2>>b2;
+    if(b1==a2 && b1==a1+b2)py
+    else if(b1==b2 && b1==a1+a2) py
+    else if(a1==a2 && a1==b1+b2) py
+    else if(a1==b2 && a1==a2+b1) py
+    else pn
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
