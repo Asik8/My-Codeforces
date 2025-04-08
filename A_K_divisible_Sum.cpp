4771417b795d8f1a97b6d6c77f150b82782bc6c6
Asik8 <motasembillah84@gmail.com>
2025-04-08 20:39:49 +0600

    8th April 2025 5 done

---
+++ A_K_divisible_Sum.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n,k,ans=0,nk;
+    cin >> n>>k;
+    nk=k;
+    while(k<n)k+=nk;
+    ll l=0,r=k+1;
+    while(l<=r){
+        ll m=l+(r-l)/2;
+        cout<<l<<' '<<r<<el
+        co(m)
+        if((m*n)>=k && !((m*n)%k)){
+            // co("Hit")
+            ans=m;
+            r=m-1;
+        } else l=m+1;
+    }
+    co(ans)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
