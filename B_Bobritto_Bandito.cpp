75e7d41df1ef5343527d764122a2e587cb04797b
Asik8 <motasembillah84@gmail.com>
2025-04-13 23:41:35 +0600

    13th April 2025 6 done

---
+++ B_Bobritto_Bandito.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n,m,l,r;
+    cin >> n>>m>>l>>r;
+    if(n==m) cout<<l<<" "<<r<<el
+    else if(m==0) cout<<0<<" "<<0<<el
+    else{
+        if(abs(r)>=abs(l)){
+            if(m<=r) cout<<0<<" "<<m<<el
+            else cout<<r-m<<" "<<r<<el
+        }
+        else{
+            if(abs(l)<=m) cout<<l<<" "<<l+m<<el
+            else cout<<-m<<" "<<0<<el
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
