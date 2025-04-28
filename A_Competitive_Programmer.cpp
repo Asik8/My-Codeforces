25c5df6eddf351eb9a5454a0e0d2f1bf35f6b390
Asik8 <motasembillah84@gmail.com>
2025-04-28 07:48:14 +0600

    28th April 2024 1 done

---
+++ A_Competitive_Programmer.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    string s;
+    cin>>s;
+    ll ev=0,zc=0,sd=0;
+    while(sz(s)){
+        char n=s.back();
+        ll rem = n-'0';
+        if(rem==0) zc++;
+        if(!(rem%2)) ev++;
+        sd+=rem;
+        s.pop_back();
+    }
+    // cout<<zc<<" "<<ev<<" "<<sd<<el
+    if(zc && ev>=2 && !(sd%3)) co("red") else co("cyan")
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
