980dc99d6cb0521788b17fb6d421253e7faf83bb
Asik8 <motasembillah84@gmail.com>
2025-05-01 09:09:27 +0600

    1st May 2025 1 done

---
+++ B_Dice_Tower.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+vec(pi) dice={{6,20},{5,19},{4,18},{3,17},{2,16},{1,15}};
+
+void asikM(){
+    ll n;
+    cin >> n;
+    if(n<15){
+        pn
+        return;
+    }
+    ll l=(n-14)%14;
+    if(l>0 && l<=6){
+        py
+        return;
+    }
+    pn
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
