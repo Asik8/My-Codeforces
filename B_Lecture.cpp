b8a9aa379a4b78cf982ce47e8affed0b9266dedc
Asik8 <motasembillah84@gmail.com>
2025-05-02 11:52:39 +0600

    2nd May 2025 4 done

---
+++ B_Lecture.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n,k;
+    cin >> n>>k;
+    map<string,string>m;
+    while(k--){
+        string a,b;
+        cin>>a>>b;
+        if(sz(a)<=sz(b)) m[a]=a; else m[a]=b;
+    }
+    forni{
+        string s;
+        cin>>s;
+        ct(m[s])
+    }
+    elc
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    // cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
