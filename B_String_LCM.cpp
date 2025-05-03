0ce7ba93d58ce9a6d2981d91925c42fd89024da0
Asik8 <motasembillah84@gmail.com>
2025-05-03 07:18:24 +0600

    3rd May 2025 2 done

---
+++ B_String_LCM.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+ll LCM(ll a,ll b){
+    return (a*(b/__gcd(a,b)));
+}
+
+void asikM(){
+    string s,a,s1;
+    cin>>s>>a;
+    ll n=sz(s),m=sz(a);
+    ll g=__gcd(n,m);
+    auto ch=[&](string s,ll l){
+        string r;
+        while(l--) r+=s;
+        return r;
+    };
+    if(ch(s,m/g)==ch(a,n/g)) co(ch(s,m/g))
+    else co(-1)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
