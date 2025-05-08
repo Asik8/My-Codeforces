d444745cf7edf048bedc19655101e81a2635bdc3
Asik8 <motasembillah84@gmail.com>
2025-05-08 12:38:37 +0600

    8th May 2025 5 done

---
+++ B_Array_Cancellation.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n;
+    cin >> n;
+    vector <ll> v(n);
+    for (auto& x:v) cin >>x; 
+    ll l=0,r=0,last=-1;
+    while(v[l]<=0) l++;
+    for(int i=n-1;i>=0;i--) if(v[i]>0){
+        last=i;
+        break;
+    }
+    r=l+1;
+    while(r<n){
+        while(v[r]<0 && v[l]>0 && l<r){
+            ll mn=min(v[l],abs(v[r]));
+            v[r]+=mn;  
+            v[l]-=mn;
+            while(l<n && v[l]<=0) l++;
+        }
+        r++;
+    }
+    ll c=0;
+    forni if(v[i]<0) c+=abs(v[i]);
+    co(c)
+    // flx(v)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
