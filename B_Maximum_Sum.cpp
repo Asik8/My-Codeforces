473b1a7463f3fca8def7df530588b739ab418e09
Asik8 <motasembillah84@gmail.com>
2025-05-10 22:31:40 +0600

    10th May 2025 3 done

--- B_Maximum_Sum.cpp
+++ B_Maximum_Sum.cpp
@@ -2,31 +2,47 @@
 using namespace std;
 #define ll long long
 #define pb push_back
-const ll p = 1e9+7;
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n,k,sum=LLONG_MIN;
+    cin >> n>>k;
+    vector <ll> v(n);
+    for (auto& x:v) cin >>x;
+    sort(v.begin(),v.end());
+    for(int i=1;i<n;i++) v[i]+=v[i-1];
+    for(int i=0;i<=k;i++){
+        ll x=(k-i);
+        // cout<<x<<" "<<n-x-1<<' '<<i<<" "<<2*i-1<<el
+        if(i==0) sum=max(sum,v[n-x-1]);
+        else sum=max(sum,v[n-x-1]-v[(2*i)-1]);
+    }
+    cout<<sum<<endl; 
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    ll t;
+    ll t=1;
     cin >> t;
-    while (t--) {
-        ll n,k,sum = 0,ans = 0,total = 0;
-        cin >> n>>k;
-        vector <ll> v(n);
-        for (auto& x:v) 
-        {
-            cin >>x;
-            total+=x;
-            sum+=x;
-            if(sum<0) sum = 0;
-            ans = max(sum,ans);
-        }     
-        total = (total%p +p)%p;
-        ans %= p;
-        ll po = 1;
-        while(k--) po = po*2%p;
-        ans = (total+ans*po-ans+p)%p;
-        cout<<ans<<endl;
-    }
+    while (t--)
+    asikM();      
     return 0;
 }
