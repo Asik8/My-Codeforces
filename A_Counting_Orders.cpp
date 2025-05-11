7fa32c8867f804e6e5c07d8d7225e8a098e3f009
Asik8 <motasembillah84@gmail.com>
2025-05-11 18:07:01 +0600

    11th May 2025 3 done

---
+++ A_Counting_Orders.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+const ll N=1e9+7;
+
+void asikM(){
+    ll n,ans=1;
+    cin >> n;
+    vector <ll> a(n),b(n);
+    for (auto& x:a) cin >>x; 
+    for (auto& x:b) cin >>x; 
+    sort(all(a));
+    sort(allr(b));
+    forni{
+        ll x=upper_bound(all(a),b[i])-a.begin();
+        ll y=n-x;
+        ans=ans*max(y-i,0LL)%N;
+    }
+    co(ans)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
