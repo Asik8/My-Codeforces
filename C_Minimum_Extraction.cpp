38170501ba691d036c61d3595f891067ba121314
Asik8 <motasembillah84@gmail.com>
2025-05-11 13:04:38 +0600

    11th May 2025 2 done

---
+++ C_Minimum_Extraction.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n;
+    cin >> n;
+    vector <ll> v(n);
+    for (auto& x:v) cin >>x; 
+    if(n==1){
+        co(v[0])
+        return;
+    }
+    ll c=0,mx=LLONG_MIN;
+    sort(all(v));
+    // flx(v)
+    c-=v[0];
+    mx=max(mx,v[0]);
+    // cout<<c<<" "<<mx<<el
+    fl(i,1,n){
+        v[i]+=c;
+        c-=v[i];
+        mx=max(mx,v[i]);
+        // cout<<"Upadeted "<<v[i]<<" "<<c<<" "<<mx<<el
+    }
+    co(mx)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
