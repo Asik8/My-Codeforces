0332302c6c5cd3d4b87cf0ea323ed2bcf69263a5
Asik8 <motasembillah84@gmail.com>
2025-05-13 23:28:41 +0600

    13th May 2025 1 done

---
+++ B_Sort_the_Subarray.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n;
+    cin >> n;
+    vector <ll> a(n),b(n);
+    for (auto& x:a) cin >>x; 
+    for (auto& x:b) cin >>x; 
+    ll l=0,r=n-1;
+    while(l<n && a[l]==b[l]) l++;
+    while(r>=0 && a[r]==b[r]) r--;
+    while(r+1<n && b[r]<=b[r+1]) r++;
+    while(l-1>=0 && b[l]>=b[l-1]) l--;
+    cout<<l+1<<" "<<r+1<<el
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
