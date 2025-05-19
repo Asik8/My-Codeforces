cf33e65401ca8fb20bdca4f2e6051ec75ce0c4fb
Asik8 <motasembillah84@gmail.com>
2025-05-19 08:23:14 +0600

    19th May 2025 1 done

--- B_Slice_to_Survive.cpp
+++ B_Slice_to_Survive.cpp
@@ -21,26 +21,21 @@ using namespace std;
 #define sz(x) x.size()
 #define vec(x) vector<x>
 
+ll cnt(ll n){
+    ll c=0;
+    while(n>1){
+        n=(n+1)/2;
+        c++;
+    }
+    return c;
+}
+
 void asikM(){
     ll n,m,a,b,c=0;
     cin >> n>>m>>a>>b;
-    while(n>1 || m>1){
-        // cout<<"Intitial:- \n";
-        // cout<<n<<" "<<m<<" "<<a<<" "<<b<<el
-        ll c1=(m-b)*n,c2=(b-1)*n;
-        ll r1=(n-a)*m,r2=(a-1)*m;
-        // cout<<c1<<" "<<c2<<" "<<r1<<" "<<r2<<el
-        if(c1>=c2 && c1>=r1 && c1>=r2) m-=(m-b);
-        else if(c2>=c1 && c2>=r1 && c2>=r2) m=(m-b)+1;
-        else if(r1>=c1 && r1>=c2 && r1>=r2) n-=(n-a);
-        else if(r2>=c1 && r2>=c2 && r2>=r1) n=(n-a)+1;
-        a=(n+1)/2;
-        b=(m+1)/2;
-        // cout<<"Updated:-\n"<<n<<" "<<m<<" "<<a<<" "<<b<<el
-        // elc
-        c++;
-    }
-    co(c+1)
+    if(n+1-a<a) a=n+1-a;
+    if(m+1-b<b) b=m+1-b;
+    co(min((1LL+cnt(n)+cnt(b)),(1LL+cnt(m)+cnt(a))))
 }
 
 int main() {
