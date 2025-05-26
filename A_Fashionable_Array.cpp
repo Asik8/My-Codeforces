2565a58a57b18af0d9505acab46f7dd921a9a5d5
Asik8 <motasembillah84@gmail.com>
2025-05-26 08:18:40 +0600

    26th May 2025 3 done

---
+++ A_Fashionable_Array.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n;
+    cin >> n;
+    vector <ll> v(n);
+    for (auto& x:v) cin >>x; 
+    sort(all(v));
+    // flx(v)
+    ll l=n-1,c1=0,c2=0;
+    while(l>=0){
+        if((v[0]+v[l])&1){
+            l--;
+            c1++;
+        }
+        else break;
+    }
+    l=0;
+    while(l<n){
+        if((v.back()+v[l])&1){
+            l++;
+            c2++;
+        }
+        else break;
+    }
+    // cout<<c1<<' '<<c2<<el
+    co(min(c1,c2))
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
