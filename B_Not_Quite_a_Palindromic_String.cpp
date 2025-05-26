52ad28df64e59c430c771a939b799f40fb0c0a6a
Asik8 <motasembillah84@gmail.com>
2025-05-26 22:39:26 +0600

    26th May 2025 6 done

--- B_Not_Quite_a_Palindromic_String.cpp
+++ B_Not_Quite_a_Palindromic_String.cpp
@@ -1,45 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define pb push_back
-#define pbs insert
-#define pi pair<ll,ll>
-#define py cout<<"YES\n";
-#define pys cout<<"Yes\n";
-#define pn cout<<"NO\n";
-#define pns cout<<"No\n";
-#define co(x1) cout<<x1<<"\n";
-#define ct(x1) cout<<x1<<" ";
-#define elc cout<<"\n";
-#define el "\n";
-#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
-#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
-#define flx(x1) for(auto x:x1) ct(x) elc
-#define forni for(int i=0;i<n;i++)
-#define all(x1) x1.begin(),x1.end()
-#define allr(x1) x1.rbegin(),x1.rend()
-#define sz(x) x.size()
-#define vec(x) vector<x>
 
-void asikM(){
-    ll n,k;
-    cin >> n>>k;
+void solve() {
+    int n, k;
+    cin >> n >> k;
     string s;
-    cin>>s;
-    ll o=count(all(s),'1'),z=count(all(s),'0');
-    cout<<o<<' '<<z<<el  
-    if(n==2){
-        pn
-        return;
+    cin >> s;
+    int z = count(s.begin(), s.end(), '0');
+    int o = n - z;
+    int tp = n / 2;
+
+    for (int x = 0; x <= k; ++x) {
+        int zn = 2 * x;
+        int on = 2 * (k - x);
+        int z_left = z - zn;
+        int o_left = o - on;
+
+        if (zn <= z && on <= o) {
+            int mm = tp - k;
+            if (min(z_left, o_left) >= mm) {
+                cout << "YES\n";
+                return;
+            }
+        }
     }
+
+    cout << "NO\n";
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    ll t=1;
-    cin >> t;
-    while (t--)
-    asikM();      
+    int t; cin >> t;
+    while (t--) solve();
     return 0;
 }
