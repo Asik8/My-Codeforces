bd5ae69cf0294f7ce897d0500dc420ebe1011f98
Asik8 <motasembillah84@gmail.com>
2025-06-19 23:39:10 +0600

    19th June 2025

---
+++ E_Sponsor_of_Your_Problems.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    string a,b;
+    ll c=0,l=0,n1=0,n2=0;
+    cin>>a>>b;
+    while(l<sz(a) && a[l]==b[l]){
+        c+=2;
+        l++;
+    }
+    while(l<sz(a)){
+        n1=(n1*10+(a[l]-'0'));
+        n2=(n2*10+(b[l]-'0'));
+        if(n2-n1==1) c++;
+        else break;
+        l++;
+    }
+    co(c)
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
