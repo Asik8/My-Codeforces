8d98b5063b062b8dea3c251125b9f613ca34917a
Asik8 <motasembillah84@gmail.com>
2025-07-27 23:33:20 +0600

    27th july 2025

---
+++ A_Recycling_Center.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pbs insert
+#define pi pair<ll,ll>
+#define py cout<<"YES\n";
+#define pys cout<<"Yes\n";
+#define pn cout<<"NO\n";
+#define pns cout<<"No\n";
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3) for(int x1=x2;x1<x3;x1++)
+#define flr(x1,x2,x3) for(int x1=x2;x1>=x3;x1--)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define sz(x) x.size()
+#define vec(x) vector<x>
+
+void asikM(){
+    ll n,c;
+    cin >> n>>c;
+    vector <ll> v(n);
+    for (auto& x:v) cin >>x; 
+    sort(all(v));
+    ll coins = 0, k = 0;
+    while (!v.empty()) {
+        ll l = (k < 63) ? (c >> k) : 0;
+        auto it = upper_bound(all(v), l);
+        if (it != v.begin()) v.erase(prev(it));
+        else{
+            coins++;
+            v.pop_back();
+        }
+        k++;
+    }
+    co(coins);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    cin >> t;
+    while (t--)
+    asikM();      
+    return 0;
+}
