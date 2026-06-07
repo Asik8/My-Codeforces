3a32d5b30628a753954d464697efda4b7af49cef
Asik8 <motasembillah84@gmail.com>
2026-06-07 21:13:03 +0600

    7th June 2026

---
+++ C_Snowfall.cpp
@@ -0,0 +1,67 @@
+/******************************************************************
+ *                   In the name of Allah                         *
+ *----------------------------------------------------------------*
+ * Author   : asikM                                               *
+ * Institute: Daffodil International University (DIU) (Alumni)    *
+ * Country  : Bangladesh - Chapai Nawabganj                       *
+ ******************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+#define yn(f) cout << (f ? "YES\n" : "NO\n");
+#define pbk push_back
+#define pi pair<ll,ll>
+#define rtc(x) {cout<<x<<endl; return;} 
+#define py {cout<<"YES\n"; return;}
+#define pys {cout<<"Yes\n"; return;}
+#define pn {cout<<"NO\n"; return;}
+#define pns {cout<<"No\n"; return;}
+#define co(x1) cout<<x1<<"\n";
+#define ct(x1) cout<<x1<<" ";
+#define elc cout<<"\n";
+#define el "\n";
+#define fl(x1,x2,x3,x4) for(int x1=x2;x1<x3;x1+=x4)
+#define flr(x1,x2,x3,x4) for(int x1=x2;x1>=x3;x1-=x4)
+#define flx(x1) for(auto x:x1) ct(x) elc
+#define acv(x1,x) for(auto x:x1)
+#define acp(x1,x,y) for(auto [x,y]:x1)
+#define forni for(int i=0;i<n;i++)
+#define all(x1) x1.begin(),x1.end()
+#define allr(x1) x1.rbegin(),x1.rend()
+#define srt(x1) sort(all(x1));
+#define sz(x) x.size()
+#define vec(x) vector<x>
+#define FastIN {ios::sync_with_stdio(false); cin.tie(NULL);}
+
+void asikM(){
+    ll n;
+    cin>>n;
+    vector<ll>v(n);
+    for(auto& x:v)cin>>x; 
+    ll c=0;
+    vector<ll>d,o;
+    forni{
+        if(!(v[i]%6)){
+            c++;
+            d.pbk(v[i]);
+        } else o.pbk(v[i]);
+    }
+    if(!c||c==n){
+        flx(v)
+        return;
+    }
+    fl(i,0,sz(o)-1,1) if((o[i]*o[i+1])%6==0)
+    sort(allr(o));
+    for(auto x:o) ct(x)
+    flx(d)
+}
+
+int main() {
+    FastIN ll t=1; 
+    cin>>t;
+    while(t--){asikM();}
+    return 0;
+}
+
